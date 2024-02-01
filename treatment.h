3fa39edcd35e075b71b316fba97f2fdc1334c892
Tanvir Ahmed <129336232+mandoingthings@users.noreply.github.com>
2024-02-01 14:53:56 -0500

    Update treatment.h

--- treatment.h
+++ treatment.h
@@ -1,1 +1,34 @@
+#ifndef TREATMENT_H
+#define TREATMENT_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "patient.h"
+
+class Surgery {
+private:
+    double surgeryCharge;
+
+public:
+    Surgery();
+    void updateSurgeryCharge(double amount);
+    double getSurgeryCharge() const;
+};
+
+class Pharmacy {
+private:
+    double pharmacyTotal;
+
+public:
+    Pharmacy();
+    void updatePharmacyTotal(double amount);
+    double getPharmacyTotal() const;
+};
+
+void displaySurgeryOptions();
+void displayMedicationOptions();
+void displayCharges(const PatientAccount& patient, const Surgery& surgery, const Pharmacy& pharmacy);
+
+#endif
 
